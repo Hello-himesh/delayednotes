84607f79f2224eefd8640b1e871d945bcebe60a9
hello-himesh <nagahimeshvuppala@gmail.com>
2025-03-16 18:49:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 async function getentries(){
 	try{
-		const respo = await fetch(entries.json);
+		const respo = await fetch('entries.json');
 		const data = await respo.json();
 		const entries = data.entries.reverse();
-		const etriesdiv = document.getElementById('entries');
+		const entriesdiv = document.getElementById('entries');
 		entriesdiv.innerHTML = '';
-		for (let file in entries){
-			const entryrespo = await fetch('noto_entries/${file}');
-			const entrytext = entryrespo.text();
+		for (let file of entries){
+			const entryrespo = await fetch(`noto_entries/${file}`);
+			const entrytext = await entryrespo.text();
 			const formatedtext = entrytext
-				.replace(/#(.#?)$/gm, '<h2>$1</h2>')
+				.replace(/#(.*?)$/gm, '<h2>$1</h2>')
 				.replace(/\n/g, '<br>');
 			const entrydiv = document.createElement('div');
 			entrydiv.innerHTML = formatedtext;
